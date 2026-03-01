74fdecf18b42bd132b4315f244f0c8a09d6ee234
ereno <eren.onat.23@cnu.edu>
2026-02-28 21:20:52 -0500

    commit

---
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,8 @@
+
+def say_hello(name):
+    """Prints a greeting."""
+    return f"Hello, {name}!"
+
+def calculate_square(number):
+    """Calculates the square of a number."""
+    return number * number
